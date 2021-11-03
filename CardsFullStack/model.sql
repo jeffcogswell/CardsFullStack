befd047aca0d43336e8daa44988425fe5a5272c7
Jeff Cogswell <jeffcogs@gmail.com>
2021-11-03 11:18:20 -0400

    Updated model to singular

--- CardsFullStack/model.sql
+++ CardsFullStack/model.sql
@@ -2,15 +2,15 @@
 
 use CardGame;
 
-create table Decks (
+create table Deck (
 	deck_id varchar(20),
     is_current boolean,
-    created_at datetime,
     username varchar(20),
+    created_at datetime,
     PRIMARY KEY (deck_id)
 );
 
-create table Cards (
+create table Card (
 	id int NOT NULL AUTO_INCREMENT,
     deck_id varchar(20),
     image varchar(200),
@@ -19,4 +19,3 @@ create table Cards (
     created_at datetime,
     PRIMARY KEY (id)
 );
-
