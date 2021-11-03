1cb778e4cb5c2a9ac11b564cc80600310dca7cd9
Jeff Cogswell <jeffcogs@gmail.com>
2021-11-03 13:37:20 -0400

    Added Connection stuff

--- CardsFullStack/Models/DAL.cs
+++ CardsFullStack/Models/DAL.cs
@@ -2,11 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
+using MySql.Data.MySqlClient;
 
 namespace CardsFullStack.Models
 {
 	public class DAL
 	{
+		public static MySqlConnection DB;
+
+
+		// Add a new deck to the Deck table
+		public static saveDeck(Deck theDeck)
+		{
+
+		}
+
+		// Get the latest deck from the Deck table
+
+
+		// Save a set of cards to the Card table for a particular deck
+
+
+		// Read all the current cards in a particular deck
 
 	}
 }
--- CardsFullStack/Startup.cs
+++ CardsFullStack/Startup.cs
@@ -20,6 +20,9 @@ namespace CardsFullStack
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			string connstring = Configuration.GetConnectionString("db");
+			Models.DAL.DB = new MySqlConnection(connstring);
+
 			services.AddControllersWithViews();
 			// In production, the Angular files will be served from this directory
 			services.AddSpaStaticFiles(configuration =>
