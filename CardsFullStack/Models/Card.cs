564803559048e95e2b03442cb9de9a63ca579f5a
Jeff Cogswell <jeffcogs@gmail.com>
2021-11-03 11:43:51 -0400

    Created response model

--- CardsFullStack/Models/Card.cs
+++ CardsFullStack/Models/Card.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
 
 namespace CardsFullStack.Models
 {
+	[Table("Card")]
 	public class Card
 	{
+		[Key]
 		public int id { get; set; }
 		public string deck_id { get; set; }
 		public string image { get; set; }
--- CardsFullStack/Models/Deck.cs
+++ CardsFullStack/Models/Deck.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
 
 namespace CardsFullStack.Models
 {
+	[Table("Deck")]
 	public class Deck
 	{
+		[ExplicitKey]
 		public string deck_id { get; set; }
 		public bool is_current { get; set; }
 		public string username { get; set; }
